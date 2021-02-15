235e9db3457eb1595e252826f4377423cbfb183d
josemorista <josemorista@id.uff.br>
2021-02-15 12:19:24 -0300

    feat: added SegmentImgService and route

--- src/modules/exams/http/routes/examsPreProcessing.routes.ts
+++ src/modules/exams/http/routes/examsPreProcessing.routes.ts
@@ -3,6 +3,7 @@ import { ClipAndConvertToImgService } from '../../services/ClipAndConvertToImgSe
 import { container } from 'tsyringe';
 import { DenoiseImgService } from '../../services/DenoiseImgService';
 import { HistogramEqualizationService } from '../../services/HistogramEqualizationService';
+import { SegmentImgService } from '../../services/SegmentImgService';
 
 const examsPreProcessingRouter = Router();
 
@@ -31,6 +32,16 @@ examsPreProcessingRouter.patch('/:id/applyHistogramEqualization', async (request
 	const { id } = request.params;
 	const histogramEqualizationService = container.resolve(HistogramEqualizationService);
 	await histogramEqualizationService.execute({
+		id,
+		method: request.body.method
+	});
+	return response.sendStatus(204);
+});
+
+examsPreProcessingRouter.patch('/:id/applySegmentation', async (request, response) => {
+	const { id } = request.params;
+	const segmentImgService = container.resolve(SegmentImgService);
+	await segmentImgService.execute({
 		id,
 		method: request.body.method
 	});
---
+++ src/modules/exams/services/SegmentImgService/index.ts
@@ -0,0 +1,44 @@
+import { inject, injectable } from 'tsyringe';
+import { IStorageProvider } from '../../../../shared/providers/StorageProvider/models/IStorageProvider';
+import { IExam } from '../../entities/models/IExam';
+import { IExamsRepository } from '../../repositories/ExamsRepository/models/IExamsRepository';
+import path from 'path';
+import { uploadConfig } from '../../../../config/upload';
+import { IOtsuSegmentationProvider } from '../../providers/OtsuSegmentationProvider/models/IOtsuSegmentationProvider';
+
+interface IHistogramEqualizationServiceDTO {
+	id: string;
+	method: IExam['segmentationMethod'];
+}
+
+@injectable()
+export class SegmentImgService {
+
+	constructor(
+		@inject('ExamsRepository')
+		private examsRepository: IExamsRepository,
+		@inject('StorageProvider')
+		private storageProvider: IStorageProvider,
+		@inject('OtsuSegmentationProvider')
+		private otsuSegmentationProvider: IOtsuSegmentationProvider
+	) { }
+
+	async execute({ method, id }: IHistogramEqualizationServiceDTO): Promise<void> {
+
+		const exam = await this.examsRepository.findById(id);
+
+		if (method === 'otsu') {
+			await this.otsuSegmentationProvider.applyOtsuSegmentation({
+				imgPath: path.resolve(uploadConfig.diskStorageProviderConfig.destination, exam.processedImgLocation),
+				outImgPath: path.resolve(uploadConfig.tmpUploadsPath, exam.processedImgLocation)
+			});
+		}
+
+		await this.storageProvider.save(exam.processedImgLocation);
+
+		await this.examsRepository.updateById(id, {
+			...exam,
+			segmentationMethod: method
+		});
+	}
+}

01007dee871aeb14abae3d2f81675f456fea6876
josemorista <josemorista@id.uff.br>
2021-02-19 01:17:47 -0300

    refactor: refactored py classifiers

--- src/shared/infra/typeorm/migrations/default/1613701144949-insertExamsClassificationsDetections.ts
+++ src/shared/infra/typeorm/migrations/default/1613701144949-insertExamsClassificationsDetections.ts
@@ -6,12 +6,14 @@ export class insertExamsClassificationsDetections1613701144949 implements Migrat
 		await queryRunner.query('insert into examsDetectionsClassifications(id, name, description) values (\'6ccb78b2-44e4-4aae-ab8b-23cde8585db9\', \'extravasamento de radiotraçador\', \'Se deve a extravasamento da radiotraçador no local da injeção\') on conflict(id) do nothing;');
 		await queryRunner.query('insert into examsDetectionsClassifications(id, name, description) values (\'8eb39b0d-4c10-4b1f-9083-c8f48666a48c\', \'implante osteoblástico\', \'Metástases ósseas, é a forma mais frequente de câncer no esqueleto \') on conflict(id) do nothing;');
 		await queryRunner.query('insert into examsDetectionsClassifications(id, name, description) values(\'e3b044b1-c1db-4d71-9c08-a9c760730fd5\', \'processo osteoarticular degenerativo\', \'Lesões ósseas/inflamatórias\') on conflict(id) do nothing;');
+		await queryRunner.query('insert into examsDetectionsClassifications(id, name, description) values (\'580a2665-d0e8-4d36-81c2-c543eafa9671\', \'acúmulo natural da bexiga\', \'acúmulo esperado do radiotraçador\') on conflict(id) do nothing;');
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query('delete from examsDetectionsClassifications where id=\'6ccb78b2-44e4-4aae-ab8b-23cde8585db9\'');
 		await queryRunner.query('delete from examsDetectionsClassifications where id=\'8eb39b0d-4c10-4b1f-9083-c8f48666a48c\'');
 		await queryRunner.query('delete from examsDetectionsClassifications where id=\'e3b044b1-c1db-4d71-9c08-a9c760730fd5\'');
+		await queryRunner.query('delete from examsDetectionsClassifications where id=\'580a2665-d0e8-4d36-81c2-c543eafa9671\'');
 	}
 
 }

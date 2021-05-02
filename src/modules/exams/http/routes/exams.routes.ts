36e6962a7a4e48521c203765f44e43f7beb6c3e4
josemorista <josemorista@id.uff.br>
2021-05-02 13:58:23 -0300

    feat: added getExams routes

--- src/modules/exams/http/routes/exams.routes.ts
+++ src/modules/exams/http/routes/exams.routes.ts
@@ -1,10 +1,31 @@
-import { Router } from 'express';
+import { response, Router } from 'express';
 import { container } from 'tsyringe';
+import { AppError } from '../../../../shared/errors/AppError';
 import { upload } from '../../../../shared/http/middlewares/upload';
+import { IExamsRepository } from '../../repositories/ExamsRepository/models/IExamsRepository';
 import { ApplySegmentationModelService } from '../../services/ApplySegmentationModelService';
 import { CreateExamService } from '../../services/CreateExamService';
 
 const examsRouter = Router();
+
+examsRouter.get('/', async (request, rseponse) => {
+	const examsRepository = container.resolve<IExamsRepository>('ExamsRepository');
+	const { patientId } = request.query;
+	if (!patientId) {
+		throw new AppError('Patient id not provided', 400);
+	}
+	return response.json(await examsRepository.findByPatient(String(patientId)));
+});
+
+examsRouter.get('/:examId', async (request, response) => {
+	const examsRepository = container.resolve<IExamsRepository>('ExamsRepository');
+	const { examId } = request.params;
+	const exam = await examsRepository.findById(examId);
+	if (exam && exam.patient?.ownerId === request.user.id) {
+		return response.json(exam);
+	}
+	throw new AppError('Unauthorized', 401);
+});
 
 examsRouter.post('/', upload.single('dcm'), async (request, response) => {
 	const createExamService = container.resolve(CreateExamService);

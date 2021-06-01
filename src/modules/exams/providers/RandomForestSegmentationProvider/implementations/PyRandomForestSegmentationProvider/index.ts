1a0ce95352c2c6d49142f1df425e06147282f1c9
josemorista <josemorista@id.uff.br>
2021-05-31 22:14:01 -0300

    perf: removed log

--- src/modules/exams/providers/RandomForestSegmentationProvider/implementations/PyRandomForestSegmentationProvider/index.ts
+++ src/modules/exams/providers/RandomForestSegmentationProvider/implementations/PyRandomForestSegmentationProvider/index.ts
@@ -11,7 +11,6 @@ export class PyRandomForestSegmentationProvider implements IRandomForestSegmenta
 			String(proba),
 			path.resolve(__dirname, 'rfModel.model')
 		]);
-		console.log(process.output.toString());
 		return JSON.parse(process.stdout.toString());
 	}
 }

378b99fe6a066f1f971b9ac381e94079ffe8664b
josemorista <josemorista@id.uff.br>
2021-06-03 17:09:49 -0300

    chore: max file size

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,7 +3,7 @@ import { env } from '../shared/env';
 
 export const uploadConfig = {
 	tmpUploadsPath: path.resolve(__dirname, '..', '..', 'tmp'),
-	maxFileSize: 2048000,
+	maxFileSize: 10240000,
 	diskStorageProviderConfig: {
 		destination: path.resolve(__dirname, '..', '..', 'uploads'),
 		publicUrl: `${env.apiUrl}/uploads`

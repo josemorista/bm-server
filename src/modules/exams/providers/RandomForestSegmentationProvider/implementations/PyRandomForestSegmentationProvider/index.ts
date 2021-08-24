73723eb25f23e90b7baa99e5dcfb06f8026d3e6a
josemorista <josemorista@id.uff.br>
2021-08-23 22:06:27 -0300

    chore: renamed random forest model

--- src/modules/exams/providers/RandomForestSegmentationProvider/implementations/PyRandomForestSegmentationProvider/index.ts
+++ src/modules/exams/providers/RandomForestSegmentationProvider/implementations/PyRandomForestSegmentationProvider/index.ts
@@ -9,7 +9,7 @@ export class PyRandomForestSegmentationProvider implements IRandomForestSegmenta
 			csvPath,
 			outDirectoryPath,
 			String(proba),
-			path.resolve(__dirname, 'rfModel.model'),
+			path.resolve(__dirname, 'randomF.model'),
 			String(shape[0]),
 			String(shape[1])
 		]);

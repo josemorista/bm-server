11c76900321274099edcf145e010800fd3b90331
josemorista <josemorista@id.uff.br>
2022-07-21 16:10:14 -0300

    dev: git-lfs

--- src/modules/exams/providers/RandomForestSegmentationProvider/implementations/PyRandomForestSegmentationProvider/index.ts
+++ src/modules/exams/providers/RandomForestSegmentationProvider/implementations/PyRandomForestSegmentationProvider/index.ts
@@ -4,15 +4,17 @@ import { spawnSync } from 'child_process';
 
 export class PyRandomForestSegmentationProvider implements IRandomForestSegmentationProvider {
 	async applyModel({ csvPath, outDirectoryPath, proba, shape }: IRandomForestApplyModelDTO): Promise<IRandomForestSegmentationProviderResponseDTO> {
+		console.time('ptime');
 		const process = spawnSync('python3', [
 			path.resolve(__dirname, 'applyModel.py'),
 			csvPath,
 			outDirectoryPath,
 			String(proba),
-			path.resolve(__dirname, 'rfmodel'),
+			path.resolve(__dirname, 'mlp100.model'),
 			String(shape[0]),
 			String(shape[1])
 		]);
+		console.timeEnd('ptime');
 		return JSON.parse(process.stdout.toString());
 	}
 }
